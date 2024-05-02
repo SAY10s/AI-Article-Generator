a937a6bce6810fc66d603a382d1929ba044aa832
SAY10s <nikodemmajzner@gmail.com>
2024-05-02 12:45:34 +0200

    Init

---
+++ src/main/java/com/example/aiarticlegenerator/AiArticleGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.aiarticlegenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiArticleGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiArticleGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aiarticlegenerator/AiArticleGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aiarticlegenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiArticleGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
